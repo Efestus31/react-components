be754ca609e549a39ff22fa0a4d2e23a1293ffa6
Efestus31 <biagiohd@gmail.com>
2024-11-12 15:42:13 +0100

    add structure components and markup and some style

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,19 @@
+export default function AppFooter() {
+
+
+
+    return (
+        <footer>
+            <div className="container">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Laboriosam, labore, placeat suscipit maxime itaque impedit pariatur explicabo
+                    , modi nobis laudantium earum dignissimos ex.
+                    Quaerat exercitationem nihil accusantium saepe sequi odio?
+
+                </p>
+            </div>
+        </footer>
+    )
+
+}
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,15 @@
+
+export default function AppHeader() {
+
+
+
+    return (
+        <header>
+            <div className="container">
+                <h2>
+                    il mio blog
+                </h2>
+            </div>
+        </header>
+    )
+}
